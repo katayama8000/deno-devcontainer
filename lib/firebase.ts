3df9b11d16075f064db6bd5228ac78bfce6445e9
katayama8000 <tattu.0310@gmail.com>
2023-10-14 13:41:04 +0900

    env

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -8,14 +8,17 @@ import { getStorage } from 'firebase/storage';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: 'AIzaSyAcR2rPqIGzfliTmCioEDwyClGr7QeF790',
-  authDomain: 'katayama-sandbox-639ef.firebaseapp.com',
-  projectId: 'katayama-sandbox-639ef',
-  storageBucket: 'katayama-sandbox-639ef.appspot.com',
-  messagingSenderId: '1013992427003',
-  appId: '1:1013992427003:web:71e59240398188c6bd0f5e',
+export const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
+
+console.table(firebaseConfig);
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
