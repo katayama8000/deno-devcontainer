75fb20894fd9667ed345857819c6a950b557cf1f
katayama800 <tattu.0310@gmail.com>
2024-01-03 08:33:50 +0000

    fmt

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,34 @@
-const main = () => {
-  console.log("Hello World");
-};
 
-main();
+class User {
+  private id: number;
+  constructor(id: number) {
+    this.id = id;
+  }
+
+  find(id: number): User {
+    return new User(id);
+  }
+}
+class UserRepository {
+  // 永続化に関する処理
+  // DBに書き込むなど
+  find(id: number): User {
+    return new User(id);
+  }
+}
+
+class UserService {
+  private userRepository: UserRepository;
+
+  constructor(userRepository: UserRepository) {
+    this.userRepository = userRepository;
+  }
+
+  public find(id: number): User {
+    return this.userRepository.find(id);
+  }
+}
+
+const userRepository = new UserRepository();
+const userService = new UserService(userRepository);
+userService.find(1);
